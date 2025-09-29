d48f61f9196254d4922d3f8a7c9aa7cfd45e9ce0
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-09-28 20:20:58 -0600

    feat: implement orthography check functionality in GptService and GptController (body)

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+
 import { orthographyCheckUseCase } from './use-cases';
+import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
   // llaama a casos de uso
-  async orthographyCheck() {
-    return await orthographyCheckUseCase();
+  async orthographyCheck(orthographyDto: OrthographyDto) {
+    return await orthographyCheckUseCase({ prompt: orthographyDto.prompt });
   }
 }
