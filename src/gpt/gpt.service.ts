9ddaea3afe9500ccc35f9fb57a01e4c8aeef1947
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-09-30 22:36:40 -0600

    feat: integrate OpenAI API for orthography check functionality

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
-  // llaama a casos de uso
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  // llama a casos de uso
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({ prompt: orthographyDto.prompt });
+    return await orthographyCheckUseCase(this.openai, {
+      prompt: orthographyDto.prompt,
+    });
   }
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,11 +1,33 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+
+  //TODO: no se puedo realizar la prueba de hacer el primer prompt a la API de
+  // GPT ya que para la key que tenemos ya no tiene créditos gratuitos y salio un error 429
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content:
+          'tu nombre es Camilo, debes responder amablemente siempre y dar tu nombre',
+      },
+      {
+        role: 'user',
+        content: prompt, // se pasa el prompt desde el body de postman
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion);
+  return completion.choices[0];
 };
