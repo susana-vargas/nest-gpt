662e24bca75ab992f71e4934f16ff2d042813349
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-10-10 00:00:11 -0600

    feat: add audio-to-text endpoint with file upload handling and validation

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,13 +1,20 @@
 import {
   Body,
   Controller,
+  FileTypeValidator,
   Get,
   HttpStatus,
+  MaxFileSizeValidator,
   Param,
+  ParseFilePipe,
   Post,
   Res,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import type { Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 import { GptService } from './gpt.service';
 import { OrthographyDto, TextToAudioDto } from './dtos';
@@ -73,5 +80,38 @@ export class GptController {
     res.setHeader('Content-Type', 'audio/mp3');
     res.status(HttpStatus.OK);
     res.sendFile(filePath);
+  }
+
+  @Post('audio-to-text')
+  //es como un "filtro" que puede ejecutar lógica antes, después o incluso reemplazar la respuesta de un método.
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './generated/uploads',
+        filename: (req, file, callback) => {
+          const fileExtension = file.originalname.split('.').pop();
+          const fileName = `${new Date().getTime()}.${fileExtension}`;
+          return callback(null, fileName);
+        },
+      }),
+    }),
+  )
+  async audioToText(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1000 * 1024 * 5,
+            message: 'file is bigger than 5 mb',
+          }),
+          new FileTypeValidator({ fileType: 'audio/*' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log({ file });
+
+    return 'done';
   }
 }
