79e376e3ab265a791ccbf69350b59fee6b9a6662
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-10-19 18:44:15 -0600

    feat: edit image

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,8 @@
 import OpenAI from 'openai';
-import { downloadImageAsPng } from 'src/helpers';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
 
 interface Options {
   prompt: string;
@@ -13,23 +16,48 @@ export const imageGenerationUseCase = async (
 ) => {
   const { prompt, originalImage, maskImage } = options;
   // TODO: verificar original image
-  const response = await openai.images.generate({
-    prompt,
-    model: 'dall-e-3',
-    n: 1,
-    size: '1024x1024',
-    quality: 'standard',
-    response_format: 'url',
-  });
-  //TODO. guardar la  imagen en FS.
-  if (response?.data?.[0].url) {
-    const url = await downloadImageAsPng(response.data[0].url);
-    return url;
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      prompt,
+      model: 'dall-e-3',
+      n: 1,
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
+    //TODO. guardar la  imagen en FS.
+    if (response?.data?.[0].url) {
+      const url = await downloadImageAsPng(response.data[0].url);
+      return url;
+    }
+
+    return {
+      url: response?.data?.[0].url, //TODO: http://localhost:3000/gpt/image-generation/09_08_16.png
+      openAIUrl: response?.data?.[0].url,
+      revised_prompt: response?.data?.[0].revised_prompt,
+    };
   }
 
-  return {
-    url: response?.data?.[0].url,
-    openAIUrl: response?.data?.[0].url,
-    revised_prompt: response?.data?.[0].revised_prompt,
-  };
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+  const response = await openai.images.edit({
+    model: 'dall-e-2',
+    prompt,
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
+    n: 1,
+    size: '1024x1024',
+    response_format: 'url',
+  });
+  if (response?.data?.[0].url) {
+    const localImagePath = await downloadImageAsPng(response.data[0].url);
+    const fileName = path.basename(localImagePath);
+    const publicUrl = `localhost:3000/${fileName}`;
+
+    return {
+      url: publicUrl, //TODO: http://localhost:3000/gpt/image-generation/09_08_16.png
+      openAIUrl: response?.data?.[0].url,
+      revised_prompt: response?.data?.[0].revised_prompt,
+    };
+  }
 };
