34a3b758d348710d37f1eddc873b326523737153
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-10-20 22:18:34 -0600

    feat: increase the size of the post request

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,9 @@ async function bootstrap() {
   );
 
   app.enableCors();
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
