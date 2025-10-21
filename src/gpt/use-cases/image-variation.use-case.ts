30e55b485c31bd9996dd4318176a5d703e6f5567
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-10-20 22:14:14 -0600

    feat: implement image variation

--- src/gpt/use-cases/image-variation.use-case.ts
+++ src/gpt/use-cases/image-variation.use-case.ts
@@ -1,4 +1,7 @@
 import OpenAI from 'openai';
+import * as fs from 'fs';
+
+import { downloadImageAsPng } from 'src/helpers';
 
 interface Options {
   baseImage: string;
@@ -9,14 +12,22 @@ export const imageVariationUseCase = async (
   options: Options,
 ) => {
   const { baseImage } = options;
-  // const response = await openai.images.createVariation({
-  //   image: baseImage,
-  //   n: 1,
-  //   size: '1024x1024',
-  // });
+  const pngImagePath = await downloadImageAsPng(baseImage, true);
+  const response = await openai.images.createVariation({
+    model: 'dall-e-2',
+    image: fs.createReadStream(pngImagePath),
+    n: 1,
+    size: '1024x1024',
+    response_format: 'url',
+  });
+  if (response?.data?.[0].url) {
+    const fileName = await downloadImageAsPng(response.data[0].url);
+    const url = `${process.env.SERVER_URL}gpt/image-generation/${fileName}`;
 
-  // return response.data[0].url;
-  console.log({ baseImage });
-
-  return baseImage;
+    return {
+      url: url,
+      openAIUrl: response?.data?.[0].url,
+      revised_prompt: response?.data?.[0].revised_prompt,
+    };
+  }
 };
--- src/helpers/download-image-as-png.ts
+++ src/helpers/download-image-as-png.ts
@@ -3,7 +3,10 @@ import * as path from 'path';
 import * as fs from 'fs';
 import sharp from 'sharp';
 
-export const downloadImageAsPng = async (url: string) => {
+export const downloadImageAsPng = async (
+  url: string,
+  fullPath: boolean = false,
+) => {
   const response = await fetch(url);
   if (!response.ok) {
     throw new InternalServerErrorException('Failed to download image');
@@ -16,10 +19,13 @@ export const downloadImageAsPng = async (url: string) => {
   const completePath = path.join(folderPath, imageNamePng);
   await sharp(buffer).png().ensureAlpha().toFile(completePath);
 
-  return imageNamePng;
+  return fullPath ? completePath : imageNamePng;
 };
 
-export const downloadBase64ImageAsPng = async (base64Image: string) => {
+export const downloadBase64ImageAsPng = async (
+  base64Image: string,
+  fullPath: boolean = false,
+) => {
   // Remover encabezado
   base64Image = base64Image.split(';base64,').pop() ?? '';
   const imageBuffer = Buffer.from(base64Image, 'base64');
@@ -33,5 +39,5 @@ export const downloadBase64ImageAsPng = async (base64Image: string) => {
   // Transformar a RGBA, png // Así lo espera OpenAI
   await sharp(imageBuffer).png().ensureAlpha().toFile(completePath);
 
-  return imageNamePng;
+  return fullPath ? completePath : imageNamePng;
 };
