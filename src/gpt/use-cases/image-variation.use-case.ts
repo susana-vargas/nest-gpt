25db0b0596d2a588a2c87f84627f6e66677391d2
susana-vargas <hernandeznerisusanaisabel@gmail.com>
2025-10-28 19:54:19 -0600

    feat: update imageVariationUseCase to use toFile for image processing

--- src/gpt/use-cases/image-variation.use-case.ts
+++ src/gpt/use-cases/image-variation.use-case.ts
@@ -1,4 +1,4 @@
-import OpenAI from 'openai';
+import OpenAI, { toFile } from 'openai';
 import * as fs from 'fs';
 
 import { downloadImageAsPng } from 'src/helpers';
@@ -12,14 +12,20 @@ export const imageVariationUseCase = async (
   options: Options,
 ) => {
   const { baseImage } = options;
+  //OpenAI recomienda que para variaciones y ediciones, no se use fs.createReadStream directo, sino toFile
   const pngImagePath = await downloadImageAsPng(baseImage, true);
+  const buffer = fs.readFileSync(pngImagePath);
   const response = await openai.images.createVariation({
     model: 'dall-e-2',
-    image: fs.createReadStream(pngImagePath),
+    image: await toFile(buffer, 'image.png', { type: 'image/png' }),
     n: 1,
     size: '1024x1024',
     response_format: 'url',
   });
+  /*
+    createReadStream() no trae MIME(metadata de la imagen)
+    toFile() agrega el MIME(metadata de la imagen) y lo convierte en un formato que OpenAI acepta (png)
+   */
   if (response?.data?.[0].url) {
     const fileName = await downloadImageAsPng(response.data[0].url);
     const url = `${process.env.SERVER_URL}gpt/image-generation/${fileName}`;
